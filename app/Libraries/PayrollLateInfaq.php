835ceca265a6f7afd5931c2ef5652e9f684bfe29
Dzulfikar Budi <buddyandreanto@gmail.com>
2026-05-28 14:42:29 +0700

    update pay

--- app/Libraries/PayrollLateInfaq.php
+++ app/Libraries/PayrollLateInfaq.php
@@ -4,33 +4,38 @@ namespace App\Libraries;
 
 class PayrollLateInfaq
 {
+    private const GRACE_MINUTES = 10;
+
     public static function getRate(int $lateMinutes): float
     {
-        if ($lateMinutes <= 0) {
+        $chargeableMinutes = self::getChargeableMinutes($lateMinutes);
+
+        if ($chargeableMinutes <= 0) {
             return 0;
         }
 
-        if ($lateMinutes < 11) {
-            return 0;
-        }
-
-        if ($lateMinutes < 30) {
+        if ($chargeableMinutes <= 30) {
             return 0.06;
         }
 
-        if ($lateMinutes <= 60) {
+        if ($chargeableMinutes <= 60) {
             return 0.12;
         }
 
-        if ($lateMinutes <= 90) {
+        if ($chargeableMinutes <= 90) {
             return 0.18;
         }
 
-        if ($lateMinutes <= 120) {
+        if ($chargeableMinutes <= 120) {
             return 0.24;
         }
 
         return 0.30;
+    }
+
+    public static function getChargeableMinutes(int $lateMinutes): int
+    {
+        return max(0, $lateMinutes - self::GRACE_MINUTES);
     }
 
     public static function calculate(int $lateMinutes, float $uangMakan, ?string $tglAktif, string $periode): int
